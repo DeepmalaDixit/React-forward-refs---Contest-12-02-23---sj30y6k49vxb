3851eb04c216da22ea749a9f4a4841766122c5c1
DeepmalaDixit <117112464+DeepmalaDixit@users.noreply.github.com>
2023-02-12 13:41:54 +0530

    Update InputField.js

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,4 +1,4 @@
-import React ,{useRef,useState,useEffect} from 'react';
+import React ,{useRef,useState,useEffect, forwardRef} from 'react';
 import '../styles/App.css';
 
 //complete the following using forward refs concept 
